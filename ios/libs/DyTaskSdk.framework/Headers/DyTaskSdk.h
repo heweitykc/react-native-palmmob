a99f7e4f758cb4237802bcef3b872a9f3e966561
贺威 <240100889@qq.com>
2020-12-30 14:09:31 +0800

    sdk加入

---
+++ ios/libs/DyTaskSdk.framework/Headers/DyTaskSdk.h
@@ -0,0 +1,19 @@
+//
+//  DyTaskSdk.h
+//  DyTaskSdk
+//
+//  Created by 雷凯 on 2020/12/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DyTaskSdk.
+FOUNDATION_EXPORT double DyTaskSdkVersionNumber;
+
+//! Project version string for DyTaskSdk.
+FOUNDATION_EXPORT const unsigned char DyTaskSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DyTaskSdk/PublicHeader.h>
+
+
+#import <DyTaskSdk/DyAdApi.h>
